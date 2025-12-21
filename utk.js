5d037584e7f56b6ad1cec603e6f0bb0ab99ba64d
UTKRANTI THAKUR <utkrantithakur@gmail.com>
2025-12-21 16:45:26 +0530

    hii

--- utk.js
+++
@@ -1,9 +0,0 @@
-let PROM1 =new Promise ((resolve , reject)=>{
-    setTimeout(() => {
-        console.log("heyy utk")
-        resolve("aahaa")
-    },2000);
-})
-PROM1.then((a)=>{
-    console.log(a)
-})
